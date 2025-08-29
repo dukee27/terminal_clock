21f4abb593bcdbe5f007a4ac6e9404c0779f4e6f
Dhruv Kachhawaha <dhruv.kachhawaha2708@gmail.com>
2025-08-29 16:12:00 +0530

    initial

--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,12 @@
 #include <iomanip>
 
 int main(){
-    
+    while(true){
+        auto now = std::chrono::system_clock::now();
+        auto time_t = std::chrono::system_clock::to_time_t(now);
+
+        system("cls");
+        std::cout<< std::put_time(std::localtime(&time_t),"%H:%M:%S");
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
 }
